7395e8be3790cc8d1c9f8cd12caa40b6768ea789
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2025-07-03 12:31:38 -0700

    fixes tests

--- aind-sharepoint-service-server/tests/test_configs.py
+++ aind-sharepoint-service-server/tests/test_configs.py
@@ -4,8 +4,6 @@ import os
 import unittest
 from unittest.mock import patch
 
-from pydantic import HttpUrl
-
 from aind_sharepoint_service_server.configs import Settings
 
 
@@ -42,19 +40,19 @@ class TestSettings(unittest.TestCase):
             tenant_id="tenant_id",
         )
         expected_resource_id = "https://graph.microsoft.com/.default"
-        expected_las_2020_url = HttpUrl(
+        expected_las_2020_url = (
             "https://graph.microsoft.com/v1.0"
             "/sites/las_site_456/lists/las_2020/items"
         )
-        expected_nsb_2023_url = HttpUrl(
+        expected_nsb_2023_url = (
             "https://graph.microsoft.com/v1.0"
             "/sites/nsb_site_123/lists/nsb_2023/items"
         )
-        expected_nsb_2019_url = HttpUrl(
+        expected_nsb_2019_url = (
             "https://graph.microsoft.com/v1.0"
             "/sites/nsb_site_123/lists/nsb_2019/items"
         )
-        expected_nsb_present_url = HttpUrl(
+        expected_nsb_present_url = (
             "https://graph.microsoft.com/v1.0"
             "/sites/nsb_site_123/lists/nsb_present/items"
         )
